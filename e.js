43e379546530357959d12fd32daf5a74ac453d00
vagrant <t.oleary@me.com>
2021-07-15 22:41:50 +0000

    test
    node e.js
    d ..
    cd ..

--- e.js
+++ e.js
@@ -1,22 +1,7 @@
-function nbYear(p0, percent, aug, p) {
-  // your code
-
-// first year = p 
-// percent = (percent / 100)
-
-// p is the population target 
-
-// aug is the influx of people 
-
-let i = 0;
-let year = 0;
-while (p0[i] < p) {
-  p0 += (p0 * (percent / 2)) + aug; 
-  year = year +1;
-  i++;
-}
-return year;
-};
 
 
+// test
+let hello = "hello"
+
+console.log(hello);
 
