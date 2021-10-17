1aed6ae763667de4e9880cf0c88e8b20c77e631d
Thomas O'Leary <t.oleary@me.com>
2021-10-16 19:52:29 -0700

    high-low

---
+++ highLow.js
@@ -0,0 +1,33 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Example:
+
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes:
+
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+const highAndLow = (numbers) => {
+  let ints = numbers.split(" ").map(Number);
+  console.log("ints:", ints);
+  let high = ints[0];
+  let low = ints[0];
+
+  for (const int of ints) {
+    if (int > high) {
+      high = int;
+    }
+    if (int < low) {
+      low = int;
+    }
+  }
+  return `${high} ${low}`;
+};
+
+console.log(highAndLow("1 2 3 4 5")); // return "5 1"
+console.log(highAndLow("1 2 -3 4 5")); // return "5 -3"
+console.log(highAndLow("1 9 3 4 -5")); // return "9 -5"
