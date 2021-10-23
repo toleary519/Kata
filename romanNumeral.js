68c6b27a7ecfae0751570cdc3399507ed09afb95
Thomas O'Leary <t.oleary@me.com>
2021-10-22 20:07:33 -0700

    need to deal with incorrect frequency

---
+++ romanNumeral.js
@@ -0,0 +1,46 @@
+// Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+// Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+// Example:
+
+// solution('XXI'); // should return 21
+// Help:
+
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
+
+function solution(roman) {
+  const rNumeral = roman.split("");
+  console.log(rNumberal);
+  let num = 0;
+
+  for (const i of rNumeral) {
+    switch (i) {
+      case "I":
+        num += 1;
+      case "V":
+        num += 5;
+      case "X":
+        num += 10;
+      case "L":
+        num += 50;
+      case "C":
+        num += 100;
+      case "D":
+        num += 500;
+      case "M":
+        num += 1000;
+    }
+  }
+
+  return num;
+}
+
+console.log(solution("MDCLXVI")); // => 1666
