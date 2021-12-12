1c5fe56d317e0648c9d9e3a5a75b0ae02943c07e
Thomas O'Leary <t.oleary@me.com>
2021-12-12 00:16:28 -0800

    meeting

---
+++ meeting.js
@@ -0,0 +1,43 @@
+// John has invited some friends. His list is:
+
+// s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+// Could you make a program that
+
+// makes this string uppercase
+// gives it sorted in alphabetical order by last name.
+// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+// So the result of function meeting(s) will be:
+
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+// It can happen that in two distinct families with the same family name two people have the same first name too.
+
+// Notes
+
+function meeting(s) {
+  const names = s.toUpperCase().replace(/\;/g, " ").split(" ");
+
+  let flippedNames = [];
+
+  for (const name of names) {
+    flippedNames.push(name.replace(/\:/g, ",").split(",").reverse().join(","));
+  }
+  console.log(
+    flippedNames
+      .sort()
+      .map((x) => `(${x})`)
+      .join("")
+  );
+  let output = flippedNames
+    .sort()
+    .map((x) => `(${x})`)
+    .join("");
+
+  return output;
+}
+s =
+  "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+
+console.log(meeting(s));
+
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
