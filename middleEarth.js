ec82a721681c319fa290d35853cb1f21f6253100
Thomas O'Leary <t.oleary@me.com>
2022-01-14 11:03:38 -0700

    kata

--- middleEarth.js
+++ middleEarth.js
@@ -30,9 +30,6 @@
 // Orcs, Men, Wargs, Goblins, Uruk Hai, Trolls, Wizards.
 // All values are non-negative integers. The resulting sum of the worth for each side will not exceed the limit of a 32-bit integer.
 
-// Output:
-// Return "Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, or "Battle Result: No victor on this battle field" if it ends in a tie.
-
 function goodVsEvil(good, evil) {
   let goodArray = good.split(" ");
   let goodWorth = 0;
