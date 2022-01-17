95a1b71970f60f3e3122b4188a09d56ab5b90790
Thomas O'Leary <t.oleary@me.com>
2022-01-17 11:06:54 -0700

    kata

--- stringOperation.js
+++ stringOperation.js
@@ -12,21 +12,6 @@
 // 5, 2, "divide"   --> 2.5
 
 // function arithmetic(a, b, operator) {
-//   if (operator === "add") {
-//     return a + b;
-//   }
-//   if (operator === "subtract") {
-//     return a - b;
-//   }
-//   if (operator === "divide") {
-//     return a / b;
-//   }
-//   if (operator === "multiply") {
-//     return a * b;
-//   }
-// }
-
-// function arithmetic(a, b, operator) {
 //   switch (operator) {
 //     case "add":
 //       return a + b;
