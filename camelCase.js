06887eafac7bb47d10dd03f0c1328ea68812e321
Thomas O'Leary <t.oleary@me.com>
2022-01-18 17:39:26 -0700

    kata

--- camelCase.js
+++ camelCase.js
@@ -1,19 +1,18 @@
+const camelCase = function (input) {
+  let camelOutput = "";
 
-const camelCase = function(input) {
- 
- let camelOutput = ""; 
-
- for (let i = 0; i < input.length; i++) {
-   if (input[i] === " ") {
-      camelOutput = camelOutput + input[i+1].toUpperCase();
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === " ") {
+      camelOutput = camelOutput + input[i + 1].toUpperCase();
       i++;
-   } else {
-     camelOutput = camelOutput + input[i];
-   }
- }
-return camelOutput;
-
+    } else {
+      camelOutput = camelOutput + input[i];
+    }
+  }
+  return camelOutput;
 };
+
+// tests
 
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
