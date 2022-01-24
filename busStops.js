d5e458958fa7b131cf625ef1c15ed53cb58f7b38
Thomas O'Leary <t.oleary@me.com>
2022-01-24 12:30:26 -0700

    kata

--- busStops.js
+++ busStops.js
@@ -4,7 +4,7 @@
 
 // Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
 
-// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+// The return integer can't be negative.
 
 let number = function (busStops) {
   let peopleLeft = 0;
